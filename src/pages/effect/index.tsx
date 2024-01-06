ea52bcc5ac561c49b3db6914629e71dee610bd0d
myduh <myduh@isoftstone.com>
2024-01-07 00:03:59 +0800

    feat: add limit useEffect scope var

--- src/pages/effect/index.tsx
+++ src/pages/effect/index.tsx
@@ -1,9 +1,25 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-export const EffectCom = () => {
+export const EffectCom = ({ isPlaying }: { isPlaying: boolean }) => {
   const dom = useRef(null);
   useEffect(() => {
     console.log(dom.current);
-  });
-  return <div ref={dom}>q111</div>;
+  }, [isPlaying]);
+  return <div ref={dom}>q111{isPlaying}</div>;
+};
+
+export const PEffectCom = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  return (
+    <>
+      <button
+        onClick={() => {
+          setIsPlaying(!isPlaying);
+        }}
+      >
+        按钮
+      </button>
+      <EffectCom isPlaying={isPlaying} />
+    </>
+  );
 };
